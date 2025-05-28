31af9c7e92f2043d59b29050666ba332affd52e5
Propfend <mellomello2030@gmail.com>
2025-05-28 16:38:38 +0000

    fixing the path from the archived file.

--- .github/actions/rust-build-archive/script.sh
+++ .github/actions/rust-build-archive/script.sh
@@ -25,25 +25,25 @@ else
 fi
 
 if [[ "$INPUT_FEATURES" == "" ]]; then
-  BUILD_CMD="cargo deb --no-build --output $INPUT_DEB"
+  DEB_CMD="cargo deb --output $INPUT_DEB.deb"
 else
-  BUILD_CMD="cargo deb --no-build --features $INPUT_FEATURES --output $INPUT_DEB"
+  DEB_CMD="cargo deb --features $INPUT_FEATURES --output $INPUT_DEB.deb"
 fi
 
 echo "🚧 Building project..."
 cargo build --profile "$INPUT_PROFILE"
 
-if [[ -n "$INPUT_ARCHIVE" ]]; then
-  tar -czf "$INPUT_ARCHIVE.tar.gz" "$BUILD_PATH/paddler"
-  echo "📦 Binary archive created: $INPUT_ARCHIVE.tar.gz"
-fi
-
 if [[ -n "$INPUT_DEB" ]]; then
   echo "🛠️ Building DEB package..."
-  $BUILD_CMD
+  $DEB_CMD
   echo "📦 DEB archive created: $INPUT_DEB.deb"
 fi
 
-cp "$BUILD_PATH/paddler" "$INPUT_BIN"
+mv "$BUILD_PATH/paddler" "$INPUT_BIN"
+
+if [[ -n "$INPUT_ARCHIVE" ]]; then
+  tar -czf "$INPUT_BIN.tar.gz" "$INPUT_BIN"
+  echo "📦 Binary archive created: $INPUT_ARCHIVE.tar.gz"
+fi
 
 echo "✅ Done."
